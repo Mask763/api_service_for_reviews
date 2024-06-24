c2f7b3a8d73f97e3799cdeb6c4807fcabb6b603b
Kirill Yakovlev <yakovlevkirevg@yandex.ru>
2024-06-24 21:41:32 +0300

    tests complete

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -13,13 +13,18 @@ v1_router = routers.DefaultRouter()
 v1_router.register('categories', CategoryViewSet, basename='category')
 v1_router.register('titles', TitleViewSet, basename='title')
 v1_router.register('genres', GenreViewSet, basename='genre')
-v1_router.register(
-    r'(?P<review_id>\d+)/comments',
-    CommentsViewSet,
-    basename='comments'
-)
-v1_router.register('', ReviewsViewSet, basename='reviews')
+# v1_router.register(
+#     r'titles/(?P<title_id>)\d+/reviews/',
+#     ReviewsViewSet,
+#     basename='reviews'
+# )
+# v1_router.register(
+#     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+#     CommentsViewSet,
+#     basename='comments'
+# )
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
+    path('v1/titles/<int:title_id>/reviews/', include('api.urlss')),
 ]
---
+++ api_yamdb/api/urlss.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import (
+    ReviewsViewSet,
+    CommentsViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'(?P<review_id>\d+)/comments',
+                CommentsViewSet,
+                basename='comments'
+                )
+router.register('', ReviewsViewSet, basename='reviews')
+urlpatterns = [
+    path('', include(router.urls)),
+]
